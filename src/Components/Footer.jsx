300413fcaccc3ed06a63faae3ab61f6306455361
Yuval Shpitzer <shpitzery10@gmail.com>
2024-10-11 18:30:33 +0300

    finished the Footer component

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,19 @@
+// basic boilerplate for a functional component in React
+import React from "react";
+const Footer = () => {
+    return (
+        <>
+        <footer>
+            <p>&copy; 2024 Event Planner Inc</p>
+            <nav>
+                <ul>
+                    <li><a href="/about">About Us</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                    <li><a href="privacy">Privacy Policy</a></li>
+                </ul>
+            </nav>
+        </footer>
+        </>
+    )
+}
+export default Footer
