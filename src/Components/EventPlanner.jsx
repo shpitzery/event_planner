479721df00eb46af439082e4c25e2289a15669ae
Yuval Shpitzer <shpitzery10@gmail.com>
2024-10-11 17:13:09 +0300

    modified event_categories section

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -12,10 +12,39 @@ const EventPlanner = () => {
                 Plan and organize your events effortlessly with Event Planner.<br /> {/*React requires all tags to be explicitly closed, even self-closing ones. Therefore, in React, you should use <br /> instead of just <br>.*/}
                 From birthdays to corporate meetings, we've got you covered.
             </p>
+            <button className='get-started-button'>Get Started</button>
 
         </section>
         <section className="events_categories">
+            <ul>
+                <h2>Social Events</h2>
+                <li>Birthday parties</li>
+                <li>Anniversary celebrations</li>
+                <li>Wedding receptions</li>
+                <li>Baby showers</li>
+                <li>Graduation parties</li>
+                <li>Family reunions</li>
+            </ul>
 
+            <ul>
+                <h2> Entertainment Events</h2>
+                <li>Concerts</li>
+                <li>Music festivals</li>
+                <li>Film screenings</li>
+                <li>Comedy shows</li>
+                <li>Art exhibitions</li>
+                <li>Cultural events</li>
+            </ul>
+            
+            <ul>
+                <h2>Community Events</h2>
+                <li>Fundraising events</li>
+                <li>Charity galas</li>
+                <li>Volunteer drives</li>
+                <li>Neighborhood block parties</li>
+                <li>Community festivals</li>
+                <li>Cultural celebrations</li>
+            </ul>
         </section>
         <section className="features">
 
